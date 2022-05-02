ddd735310b0e600f5ae17f0923099081829d0fb9
SplashMatrix <kautilya@growsimplee.com>
2022-05-02 21:27:57 +0530

    Logic code updated

--- eb_sqs/worker/service.py
+++ eb_sqs/worker/service.py
@@ -19,14 +19,14 @@ from eb_sqs.worker.worker_factory import WorkerFactory
 
 logger = logging.getLogger(__name__)
 
-MESSAGES_RECEIVED = django.dispatch.Signal(providing_args=['messages'])
-MESSAGES_PROCESSED = django.dispatch.Signal(providing_args=['messages'])
-MESSAGES_DELETED = django.dispatch.Signal(providing_args=['messages'])
+MESSAGES_RECEIVED = django.dispatch.Signal(providing_args=["messages"])
+MESSAGES_PROCESSED = django.dispatch.Signal(providing_args=["messages"])
+MESSAGES_DELETED = django.dispatch.Signal(providing_args=["messages"])
 
 
 class WorkerService(object):
-    _PREFIX_STR = 'prefix:'
-    _RECEIVE_COUNT_ATTRIBUTE = 'ApproximateReceiveCount'
+    _PREFIX_STR = "prefix:"
+    _RECEIVE_COUNT_ATTRIBUTE = "ApproximateReceiveCount"
 
     def __init__(self):
         # type: () -> None
@@ -40,12 +40,14 @@ class WorkerService(object):
         self.write_healthcheck_file()
         self._last_healthcheck_time = timezone.now()
 
-        logger.debug('[django-eb-sqs] Connecting to SQS: {}'.format(', '.join(queue_names)))
+        logger.debug(
+            "[django-eb-sqs] Connecting to SQS: {}".format(", ".join(queue_names))
+        )
 
         sqs = boto3.resource(
-            'sqs',
+            "sqs",
             region_name=settings.AWS_REGION,
-            config=Config(retries={'max_attempts': settings.AWS_MAX_RETRIES}),
+            config=Config(retries={"max_attempts": settings.AWS_MAX_RETRIES}),
             aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
             aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
         )
@@ -55,31 +57,60 @@ class WorkerService(object):
 
         queue_prefixes = [prefix.split(self._PREFIX_STR)[1] for prefix in prefixes]
         static_queues = queues
-        last_update_time = timezone.now() - timedelta(seconds=settings.REFRESH_PREFIX_QUEUES_S)
+        last_update_time = timezone.now() - timedelta(
+            seconds=settings.REFRESH_PREFIX_QUEUES_S
+        )
 
-        logger.debug('[django-eb-sqs] Connected to SQS: {}'.format(', '.join(queue_names)))
+        logger.debug(
+            "[django-eb-sqs] Connected to SQS: {}".format(", ".join(queue_names))
+        )
 
         worker = WorkerFactory.default().create()
 
-        logger.info('[django-eb-sqs] WAIT_TIME_S = {}'.format(settings.WAIT_TIME_S))
-        logger.info('[django-eb-sqs] NO_QUEUES_WAIT_TIME_S = {}'.format(settings.NO_QUEUES_WAIT_TIME_S))
-        logger.info('[django-eb-sqs] MAX_NUMBER_OF_MESSAGES = {}'.format(settings.MAX_NUMBER_OF_MESSAGES))
-        logger.info('[django-eb-sqs] AUTO_ADD_QUEUE = {}'.format(settings.AUTO_ADD_QUEUE))
-        logger.info('[django-eb-sqs] QUEUE_PREFIX = {}'.format(settings.QUEUE_PREFIX))
-        logger.info('[django-eb-sqs] DEFAULT_QUEUE = {}'.format(settings.DEFAULT_QUEUE))
-        logger.info('[django-eb-sqs] DEFAULT_MAX_RETRIES = {}'.format(settings.DEFAULT_MAX_RETRIES))
-        logger.info('[django-eb-sqs] REFRESH_PREFIX_QUEUES_S = {}'.format(settings.REFRESH_PREFIX_QUEUES_S))
+        logger.info("[django-eb-sqs] WAIT_TIME_S = {}".format(settings.WAIT_TIME_S))
+        logger.info(
+            "[django-eb-sqs] NO_QUEUES_WAIT_TIME_S = {}".format(
+                settings.NO_QUEUES_WAIT_TIME_S
+            )
+        )
+        logger.info(
+            "[django-eb-sqs] MAX_NUMBER_OF_MESSAGES = {}".format(
+                settings.MAX_NUMBER_OF_MESSAGES
+            )
+        )
+        logger.info(
+            "[django-eb-sqs] AUTO_ADD_QUEUE = {}".format(settings.AUTO_ADD_QUEUE)
+        )
+        logger.info("[django-eb-sqs] QUEUE_PREFIX = {}".format(settings.QUEUE_PREFIX))
+        logger.info("[django-eb-sqs] DEFAULT_QUEUE = {}".format(settings.DEFAULT_QUEUE))
+        logger.info(
+            "[django-eb-sqs] DEFAULT_MAX_RETRIES = {}".format(
+                settings.DEFAULT_MAX_RETRIES
+            )
+        )
+        logger.info(
+            "[django-eb-sqs] REFRESH_PREFIX_QUEUES_S = {}".format(
+                settings.REFRESH_PREFIX_QUEUES_S
+            )
+        )
 
         while not self._exit_gracefully:
-            if len(queue_prefixes) > 0 and \
-                    timezone.now() - timedelta(seconds=settings.REFRESH_PREFIX_QUEUES_S) > last_update_time:
-                queues = static_queues + self.get_queues_by_prefixes(sqs, queue_prefixes)
+            if (
+                len(queue_prefixes) > 0
+                and timezone.now() - timedelta(seconds=settings.REFRESH_PREFIX_QUEUES_S)
+                > last_update_time
+            ):
+                queues = static_queues + self.get_queues_by_prefixes(
+                    sqs, queue_prefixes
+                )
                 last_update_time = timezone.now()
-                logger.debug('[django-eb-sqs] Updated SQS queues: {}'.format(
-                    ', '.join([queue.url for queue in queues])
-                ))
+                logger.info(
+                    "[django-eb-sqs] Updated SQS queues: {}".format(
+                        ", ".join([queue.url for queue in queues])
+                    )
+                )
 
-            logger.debug('[django-eb-sqs] Processing {} queues'.format(len(queues)))
+            logger.debug("[django-eb-sqs] Processing {} queues".format(len(queues)))
             if len(queues) == 0:
                 sleep(settings.NO_QUEUES_WAIT_TIME_S)
             else:
@@ -94,17 +125,16 @@ class WorkerService(object):
 
             try:
                 messages = self.poll_messages(queue)
-                logger.debug('[django-eb-sqs] Polled {} messages'.format(len(messages)))
+                logger.debug("[django-eb-sqs] Polled {} messages".format(len(messages)))
 
                 self._send_signal(MESSAGES_RECEIVED, messages=messages)
 
                 msg_entries = []
                 for msg in messages:
                     self._execute_user_code(lambda: self._process_message(msg, worker))
-                    msg_entries.append({
-                            'Id': msg.message_id,
-                            'ReceiptHandle': msg.receipt_handle
-                    })
+                    msg_entries.append(
+                        {"Id": msg.message_id, "ReceiptHandle": msg.receipt_handle}
+                    )
 
                 self._send_signal(MESSAGES_PROCESSED, messages=messages)
 
@@ -112,15 +142,35 @@ class WorkerService(object):
 
                 self._send_signal(MESSAGES_DELETED, messages=messages)
             except ClientError as exc:
-                error_code = exc.response.get('Error', {}).get('Code', None)
-                if error_code == 'AWS.SimpleQueueService.NonExistentQueue' and queue not in static_queues:
-                    logger.debug('[django-eb-sqs] Queue was already deleted {}: {}'.format(queue.url, exc), exc_info=True)
+                error_code = exc.response.get("Error", {}).get("Code", None)
+                if (
+                    error_code == "AWS.SimpleQueueService.NonExistentQueue"
+                    and queue not in static_queues
+                ):
+                    logger.debug(
+                        "[django-eb-sqs] Queue was already deleted {}: {}".format(
+                            queue.url, exc
+                        ),
+                        exc_info=True,
+                    )
                 else:
-                    logger.warning('[django-eb-sqs] Error polling queue {}: {}'.format(queue.url, exc), exc_info=True)
+                    logger.warning(
+                        "[django-eb-sqs] Error polling queue {}: {}".format(
+                            queue.url, exc
+                        ),
+                        exc_info=True,
+                    )
             except Exception as exc:
-                logger.warning('[django-eb-sqs] Error polling queue {}: {}'.format(queue.url, exc), exc_info=True)
+                logger.warning(
+                    "[django-eb-sqs] Error polling queue {}: {}".format(queue.url, exc),
+                    exc_info=True,
+                )
 
-            if timezone.now() - timedelta(seconds=settings.MIN_HEALTHCHECK_WRITE_PERIOD_S) > self._last_healthcheck_time:
+            if (
+                timezone.now()
+                - timedelta(seconds=settings.MIN_HEALTHCHECK_WRITE_PERIOD_S)
+                > self._last_healthcheck_time
+            ):
                 self.write_healthcheck_file()
                 self._last_healthcheck_time = timezone.now()
 
@@ -130,40 +180,52 @@ class WorkerService(object):
             response = queue.delete_messages(Entries=msg_entries)
 
             # logging
-            failed = response.get('Failed', [])
+            failed = response.get("Failed", [])
             num_failed = len(failed)
             if num_failed > 0:
-                logger.warning('[django-eb-sqs] Failed deleting {} messages: {}'.format(num_failed, failed))
+                logger.warning(
+                    "[django-eb-sqs] Failed deleting {} messages: {}".format(
+                        num_failed, failed
+                    )
+                )
 
     def poll_messages(self, queue):
         # type: (Queue) -> list
         return queue.receive_messages(
             MaxNumberOfMessages=settings.MAX_NUMBER_OF_MESSAGES,
             WaitTimeSeconds=settings.WAIT_TIME_S,
-            AttributeNames=[self._RECEIVE_COUNT_ATTRIBUTE]
+            AttributeNames=[self._RECEIVE_COUNT_ATTRIBUTE],
         )
 
     def _send_signal(self, dispatch_signal, messages):
         # type: (django.dispatch.Signal, list) -> None
         if dispatch_signal.has_listeners(sender=self.__class__):
-            self._execute_user_code(lambda: dispatch_signal.send(sender=self.__class__, messages=messages))
+            self._execute_user_code(
+                lambda: dispatch_signal.send(sender=self.__class__, messages=messages)
+            )
 
     def _process_message(self, msg, worker):
         # type: (Message, Worker) -> None
-        logger.debug('[django-eb-sqs] Read message {}'.format(msg.message_id))
+        logger.debug("[django-eb-sqs] Read message {}".format(msg.message_id))
         try:
             receive_count = int(msg.attributes[self._RECEIVE_COUNT_ATTRIBUTE])
 
             if receive_count > 1:
-                logger.warning('[django-eb-sqs] SQS re-queued message {} times - msg: {}'.format(
-                    receive_count, msg.body
-                ))
+                logger.warning(
+                    "[django-eb-sqs] SQS re-queued message {} times - msg: {}".format(
+                        receive_count, msg.body
+                    )
+                )
 
             worker.execute(msg.body)
 
-            logger.debug('[django-eb-sqs] Processed message {}'.format(msg.message_id))
+            logger.debug("[django-eb-sqs] Processed message {}".format(msg.message_id))
         except ExecutionFailedException as exc:
-            logger.warning('[django-eb-sqs] Handling message {} got error: {}'.format(msg.message_id, repr(exc)))
+            logger.warning(
+                "[django-eb-sqs] Handling message {} got error: {}".format(
+                    msg.message_id, repr(exc)
+                )
+            )
 
     @staticmethod
     def _execute_user_code(function):
@@ -172,11 +234,15 @@ class WorkerService(object):
             with django_db_management():
                 function()
         except Exception as exc:
-            logger.error('[django-eb-sqs] Unhandled error: {}'.format(exc), exc_info=True)
+            logger.error(
+                "[django-eb-sqs] Unhandled error: {}".format(exc), exc_info=True
+            )
 
     def get_queues_by_names(self, sqs, queue_names):
         # type: (ServiceResource, list) -> list
-        return [sqs.get_queue_by_name(QueueName=queue_name) for queue_name in queue_names]
+        return [
+            sqs.get_queue_by_name(QueueName=queue_name) for queue_name in queue_names
+        ]
 
     def get_queues_by_prefixes(self, sqs, prefixes):
         # type: (ServiceResource, list) -> list
@@ -189,9 +255,9 @@ class WorkerService(object):
 
     def write_healthcheck_file(self):
         # type: () -> None
-        with open(settings.HEALTHCHECK_FILE_NAME, 'w') as file:
+        with open(settings.HEALTHCHECK_FILE_NAME, "w") as file:
             file.write(timezone.now().isoformat())
 
     def _exit_called(self, signum, frame):
-        logger.info('[django-eb-sqs] Termination signal called: {}'.format(signum))
+        logger.info("[django-eb-sqs] Termination signal called: {}".format(signum))
         self._exit_gracefully = True
