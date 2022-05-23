a6009db710ea626d26767affeb0dfdfea7cd6e3d
gs-sinha <89183774+gs-sinha@users.noreply.github.com>
2022-05-23 15:13:19 +0530

    Update process_queue.py

--- eb_sqs/management/commands/process_queue.py
+++ eb_sqs/management/commands/process_queue.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, unicode_literals
 from django.core.management import BaseCommand, CommandError
 
 from eb_sqs.worker.service import WorkerService
-
+from eb_sqs.settings import CONSUME_QUEUE_NAMES as CQN
 
 class Command(BaseCommand):
     help = 'Command to process tasks from one or more SQS queues'
@@ -14,9 +14,11 @@ class Command(BaseCommand):
                             help='Name of queues to process, separated by commas')
 
     def handle(self, *args, **options):
-        if not options['queue_names']:
+        queue_names_str = options.get('queue_names',CQN)
+        
+        if queue_names_str == "":
             raise CommandError('Queue names (--queues) not specified')
-
-        queue_names = [queue_name.rstrip() for queue_name in options['queue_names'].split(',')]
+            
+        queue_names = [queue_name.rstrip() for queue_name in queue_names_str.split(',')]
 
         WorkerService().process_queues(queue_names)
